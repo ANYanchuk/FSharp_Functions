272bfade686e7f808132fef7ce8ecf454c2b8552
Andrey Yanchuk <and.yanchuk@gmail.com>
2020-02-20 11:02:20 +0200

    Bin and Dec NS Converter

---
+++ BinAndDecConverter.fs
@@ -0,0 +1,20 @@
+﻿open System.Text.RegularExpressions
+
+let binToDec (origin:string) =
+    if(Regex.IsMatch (origin, "^[10]*$")) then
+        origin.ToCharArray()
+        |> List.ofArray
+        |> List.rev
+        |> List.indexed
+        |> List.fold (fun a b -> a + (int(snd b) - 48)  * pown 2 (fst b)) 0
+        |> Some
+    else
+        None
+
+let decToBin = function 
+    | 0 -> Some "0"
+    | origin when origin < 0 -> None
+    | origin -> origin 
+                |> Seq.unfold (function 0 -> None | i -> Some (string (i % 2), i / 2)) 
+                |> Seq.reduce (fun a b -> b + a) 
+                |> Some
